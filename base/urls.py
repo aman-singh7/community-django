f50fc12d85537576e658db29ecf6d4586cde7f86
Aman <aman2@me.iitr.ac.in>
2022-10-29 19:11:44 +0530

    feat: add login and register screen
    Signed-off-by: Aman <aman2@me.iitr.ac.in>

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('login/', views.loginUser, name="login"),
+    path('register/', views.registerUser, name="register"),
+]
