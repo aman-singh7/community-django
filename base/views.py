becccee5d3db3fe98dfe25680bea94e88b4be8c4
Aman <aman2@me.iitr.ac.in>
2022-11-04 21:17:07 +0530

    feat: add topics and acitivity route for mobile screen dimension
    Signed-off-by: Aman <aman2@me.iitr.ac.in>

--- base/views.py
+++ base/views.py
@@ -199,3 +199,15 @@ def updateUser(request):
 
     context = {'user': user, 'form': form}
     return render(request, 'base/update_user.html', context)
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+
+    context = {'topics': topics}
+    return render(request, 'base/topics.html', context)
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    context = {'room_messages': room_messages}
+    return render(request,'base/activity.html', context)
